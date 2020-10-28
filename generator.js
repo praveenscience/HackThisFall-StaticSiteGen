a514f6039dabcff27fd9990a3595e7ab392da0a0
Praveen Kumar <praveenscience@gmail.com>
2020-10-28 11:59:01 +0000

    Implemented the generator.

--- generator.js
+++ generator.js
@@ -1,5 +1,15 @@
+const fs = require("fs");
+const { generateHeader, generateFooter } = require("./static");
+const generateBody = require("./event");
+
 const generateEvent = (ev, idx) => {
   console.log(`Processing Event #${idx + 1}: ${ev.Title}`);
+  const finalHTML = generateHeader() + generateBody(ev) + generateFooter();
+  if (!fs.existsSync("./events")) {
+    fs.mkdirSync("events");
+  }
+  fs.writeFileSync(`./events/event-${idx + 1}.html`, finalHTML);
+  console.log(`Created Event #${idx + 1}: ${ev.Title}`);
 };
 
 module.exports = generateEvent;
