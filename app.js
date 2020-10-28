5aa949f2d14cbdfa8608ebbd04b620f1bfa95ecb
Praveen Kumar <praveenscience@gmail.com>
2020-10-28 11:39:24 +0000

    Successfully displayed all the events.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const Events = require("./events.json");
 console.log("Hello HackThisFall!");
 console.log("There are " + Events.length + " events available.");
+console.log("Listing all the events.");
+Events.forEach((ev, idx) => {
+  console.log(`Event #${idx + 1}: ${ev.Title}`);
+});
