ad0289ef07ba8fc80700089cceb9c4d23564939f
Praveen Kumar <praveenscience@gmail.com>
2020-10-28 11:41:26 +0000

    Created a generator function for the events.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Events = require("./events.json");
+const generateEvent = require("./generator");
 console.log("Hello HackThisFall!");
 console.log("There are " + Events.length + " events available.");
 console.log("Listing all the events.");
-Events.forEach((ev, idx) => {
-  console.log(`Event #${idx + 1}: ${ev.Title}`);
-});
+Events.forEach(generateEvent);
+console.log("Generated pages for " + Events.length + " events.");
---
+++ generator.js
@@ -0,0 +1,5 @@
+const generateEvent = (ev, idx) => {
+  console.log(`Processing Event #${idx + 1}: ${ev.Title}`);
+};
+
+module.exports = generateEvent;
