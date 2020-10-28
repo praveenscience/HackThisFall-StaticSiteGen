31974686cbee757b2033070038b08eafa7ea7c6d
Praveen Kumar <praveenscience@gmail.com>
2020-10-28 12:15:19 +0000

    Updated events into docs in code.

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const genHome = require("./genhome");
 console.log("Hello HackThisFall!");
 console.log("There are " + Events.length + " events available.");
 console.log("Processing home page.");
-if (!fs.existsSync("./events")) {
-  fs.mkdirSync("events");
+if (!fs.existsSync("./docs")) {
+  fs.mkdirSync("docs");
 }
-fs.writeFileSync(`./events/index.html`, genHome(Events));
+fs.writeFileSync(`./docs/index.html`, genHome(Events));
 console.log("Generated home page.");
 console.log("Listing all the events.");
 Events.forEach(generateEvent);
--- generator.js
+++ generator.js
@@ -5,7 +5,7 @@ const generateBody = require("./event");
 const generateEvent = (ev, idx) => {
   console.log(`Processing Event #${idx + 1}: ${ev.Title}`);
   const finalHTML = generateHeader() + generateBody(ev) + generateFooter();
-  fs.writeFileSync(`./events/event-${idx + 1}.html`, finalHTML);
+  fs.writeFileSync(`./docs/event-${idx + 1}.html`, finalHTML);
   console.log(`Created Event #${idx + 1}: ${ev.Title}`);
 };
 
